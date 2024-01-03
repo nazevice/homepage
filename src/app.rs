420bbea8e8526dc909e143ee22aad1f393862596
Naze Vice <hello@kommeterr.dev>
2024-01-03 12:04:47 +0100

    Add avatar component

--- src/app.rs
+++ src/app.rs
@@ -24,84 +24,9 @@ pub fn App() -> impl IntoView {
 
 #[component]
 fn HomePage() -> impl IntoView {
-    let el = create_node_ref::<Div>();
-
-    let inner_width = use_window()
-        .as_ref()
-        .map(|w| w.inner_width().unwrap().as_f64().unwrap())
-        .unwrap_or(0.0);
-
-    let UseDraggableReturn { x, y, style, .. } = use_draggable_with_options(
-        el,
-        UseDraggableOptions::default()
-            .initial_value(Position {
-                x: inner_width / 2.3,
-                y: 300.0,
-            })
-            .prevent_default(true),
-    );
-
-    const LEPTOS_HELPFUL: [(&'static str, &'static str); 2] = [
-        ("https://book.leptos.dev", "Leptos Docs"),
-        ("https://discord.gg/umUZ5Y8F7u", "Join the Official Discord"),
-    ];
-
     view! {
         <main class="min-h-screen py-20 w-full space-y-8">
-            <div class="text-center font-robotomono space-y-2">
-                <p class="text-center font-robotomono text-3xl">Welcome to Leptos!</p>
-                <p>Set up a client side rendered Leptos app with one command</p>
-                <Avatar></Avatar>
-            </div>
-            <div class="flex space-x-20 justify-center">
-                {LEPTOS_HELPFUL
-                    .into_iter()
-                    .enumerate()
-                    .map(|(idx, (link, title))| {
-                        view! {
-                            <a class: bg - color - 1 = move || idx == 0 class : bg - color - 2 =
-                            move || idx != 0 href = { link } target = "_blank" rel = "noreferrer" >
-                            < p > { title } </ p > </ a >
-                        }
-                    })
-                    .collect::<Vec<_>>()}
-            </div>
-
-            <div
-                node_ref=el
-                class="font-robotomono px-4 py-2 space-y-4 border border-gray-400/30 rounded shadow hover:shadow-lg fixed select-none cursor-move z-24"
-                style=move || format!("touch-action: none; {}", style())
-            >
-                <div>
-                    <p>
-                        Contribute to the
-                        <a
-                            class="hover:no-underline underline"
-                            href="https://github.com/friendlymatthew/create-leptos-csr"
-                            target="_blank"
-                            rel="noreferrer"
-                        >
-                            repository!
-                        </a>
-                    </p>
-
-                    <p>
-                        Or, submit
-                        <a
-                            class="hover:no-underline underline"
-                            href="https://github.com/friendlymatthew/create-leptos-csr/issues"
-                            target="_blank"
-                            rel="noreferrer"
-                        >
-                            a issue
-                        </a>
-                    </p>
-                </div>
-                <div class="text-sm flex justify-between">
-                    I am {move || x().round()} , {move || y().round()}
-                    <p class="opacity-100">"🙈🙉🙊"</p>
-                </div>
-            </div>
+            <Avatar></Avatar>
         </main>
     }
 }
--- src/components/avatar.rs
+++ src/components/avatar.rs
@@ -3,6 +3,12 @@ use leptos::*;
 #[component]
 pub fn Avatar() -> impl IntoView {
     view! {
-        <div>Test</div>
+        <div class="flex items-center bg-white rounded-full shadow-md overflow-hidden">
+            <img class="rounded-full" src="/../public/images/avatar.jpg" alt="my potrait" height="256px" widht="256px" />
+            <div class="flex flex-col justify-center px-4 py-2">
+                <span class="font-semibold text-lg text-gray-800">Robert Kommeter</span>
+                <span class="text-sm text-gray-500">Software engineer</span>
+            </div>
+        </div>
     }
 }
